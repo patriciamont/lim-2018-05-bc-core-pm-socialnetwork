4bfd00d115a2c13943aac73b526e4fb4074bcc11
patriciamont <fiorellamonteverde@gmail.com>
2018-07-25 13:00:37 -0500

    agregando funciones globales para login

--- src/js/signin.js
+++ src/js/signin.js
@@ -1,4 +1,28 @@
-const btnSignin = document.getElementById("btnSignin");
+//********ESTE DOCUMENTO MANIPULA AL index.html********//
+
+//VARIABLES
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const btnSignin = document.getElementById('btnSignin')
+const btnFacebook = document.getElementById('btnFacebook')
+const btnGoogle = document.getElementById('btnGoogle')
+
+
+btnSignin.addEventListener('click',()=>{
+    signIn(email.value,password.value)
+})
+
+btnFacebook.addEventListener('click',()=>{
+    signFacebook()
+})
+
+btnGoogle.addEventListener('click',()=>{
+    signGoogle()
+})
+
+
+
+/* const btnSignin = document.getElementById("btnSignin");
 btnSignin.addEventListener('click', () => {
     firebase.auth().signInWithEmailAndPassword(email.value, password.value)
         .then(function () {
@@ -7,4 +31,4 @@ btnSignin.addEventListener('click', () => {
         .catch(function (error) {
             console.log('Contraseña incorrecta');
         });
-})
+}) */
